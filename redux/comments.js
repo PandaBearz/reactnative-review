1a09f6ba3959732119193ef118a925d9e39a880a
Samuel VanCuren <samuel@vancuren.net>
2021-08-09 20:04:15 -0400

    End of Week 2 Workshop Still not Updating The React Store

--- redux/comments.js
+++ redux/comments.js
@@ -1,9 +1,12 @@
 import * as ActionTypes from './ActionTypes';
 
-export const comments = (state = { errMess: null, comments: []}, action) => {
+export const comments = (state = { errMess: null, comments: [], comment: ""}, action) => {
     switch (action.type) {
         case ActionTypes.ADD_COMMENTS:
             return {...state, errMess: null, comments: action.payload};
+
+        case ActionTypes.ADD_COMMENT:
+                return {...state, comment: {...action.payload, id: comments.length}}
 
         case ActionTypes.COMMENTS_FAILED:
             return {...state, errMess: action.payload};
