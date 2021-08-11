33e02124071dcb581c64d7ba48e09b500160b285
Samuel VanCuren <samuel@vancuren.net>
2021-08-11 17:03:09 -0400

    End Of Week 3 Workshop Task 2 - Plus Major HeartAttack But Quick Fix With The BaseUrl This Time Not Stupid Microsoft Updates. Ugh Im So Happy

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,4 @@
-export const baseUrl = 'http://192.168.132.143:3001/';
+//VCG5 Network IP
+// export const baseUrl = 'http://192.168.132.143:3001/';
+//VCG Network IP
+export const baseUrl = 'http://192.168.1.242:3001/';
